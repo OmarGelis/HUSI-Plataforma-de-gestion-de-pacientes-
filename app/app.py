5a8dd7bfc5b59533bedf583d2902e4055358f541
OmarGelis <omar.gelis.baena@gmail.com>
2021-10-19 15:40:39 -0500

    se agrega las paginas restantes de la dashboard pacientes y configuramos la plantilla.

--- app/app.py
+++ app/app.py
@@ -5,12 +5,28 @@ app =  Flask(__name__)
 def login():
     return render_template('login.html')
 
+@app.route('/registrar') 
+def registrar():
+    return render_template('registrar.html') 
+
 @app.route('/dashboardPacientes') 
 def dashboardPacientes():
     return render_template('dashboardPacientes.html')
 
-@app.route('/registrar') 
-def registrar():
-    return render_template('registrar.html')        
+@app.route('/agendarCita') 
+def agendarCita():
+    return render_template('agendarCita.html')
+
+@app.route('/historiaClinica') 
+def historiaClinica():
+    return render_template('historiaClinica.html')
+
+@app.route('/perfilPaciente') 
+def perfilPaciente():
+    return render_template('perfilPacientes.html')
+
+@app.route('/configuracionPaciente') 
+def configuracionPaciente():
+    return render_template('configuraconPacientes.html')            
 
 app.run(debug=True)    
